93cf7229ad387a2ec332bc7a7ca6251f5fdb4385
Pratan naimjoi <52064201+tanpattaya@users.noreply.github.com>
2019-12-17 15:21:59 +0700

    Add files via upload

--- liff-starter.js
+++ liff-starter.js
@@ -1,194 +1,88 @@
 window.onload = function (e) {
-    liff.init(function () {
-        getP();
+    liff.init(function (data) {
+        initializeApp(data);
     });
 };
 
-function getP(){
-    var tipe = getParameterByName('type')
-    if (!tipe) {
-        document.getElementById('textx').addEventListener('click', function () {
-            liff.sendMessages([{
-                type: 'text',
-                text: atob('bGluZTovL2FwcC8xNjAyNjg3MzA4LUdYcTRWdms5P3R5cGU9dGV4dCZ0ZXh0PVlvdXIlMjBUZXh0Cgp0eXBlID0+IHRleHQKdGV4dCA9PiB5b3VyIHRleHQ=')
-            }]).then(function () {
-                liff.closeWindow();
-            });
-        });
-        document.getElementById('imagex').addEventListener('click', function () {
-            liff.sendMessages([{
-                type: 'text',
-                text: atob('bGluZTovL2FwcC8xNjAyNjg3MzA4LUdYcTRWdms5P3R5cGU9aW1hZ2UmaW1nPWh0dHBzOi8vd2FsbHBhcGVyc3R1ZGlvMTAuY29tL3N0YXRpYy93cGRiL3dhbGxwYXBlcnMvMTAwMHg1NjMvMTY4ODkxLmpwZwoKdHlwZSA9PiBpbWFnZQppbWcgPT4gTGluayAobXVzdCBiZSBIVFRQUyk=')
-            }]).then(function () {
-                liff.closeWindow();
-            });
-        });
-        document.getElementById('videox').addEventListener('click', function () {
-            liff.sendMessages([{
-                type: 'text',
-                text: atob('bGluZTovL2FwcC8xNjAyNjg3MzA4LUdYcTRWdms5P3R5cGU9dmlkZW8mb2N1PWh0dHBzOi8vdGlueXVybC5jb20veThvZzNvcjUmcGl1PWh0dHBzOi8vaW1hZ2VzNi5hbHBoYWNvZGVycy5jb20vNzEwL3RodW1iLTM1MC03MTAxMzIucG5nCgp0eXBlID0+IHZpZGVvCm9jdSA9PiB2aWRlbyB1cmwKcGl1ID0+IHByZXZpZXcgaW1hZ2U=')
-            }]).then(function () {
-                liff.closeWindow();
-            });
-        });
-        document.getElementById('audiox').addEventListener('click', function () {
-            liff.sendMessages([{
-                type: 'text',
-                text: atob('bGluZTovL2FwcC8xNjAyNjg3MzA4LUdYcTRWdms5P3R5cGU9YXVkaW8mbGluaz1odHRwczovL3BsYXRlbGV0cy5mdW4vcHVibGljL3NvdW5kcy9tdXNpYy5tcDM=')
-            }]).then(function () {
-                liff.closeWindow();
-            });
-        });
-        document.getElementById('stickerx').addEventListener('click', function () {
-            liff.sendMessages([{
-                type: 'text',
-                text: atob('V2l0aCBBbmltYXRpb246CmxpbmU6Ly9hcHAvMTYwMjY4NzMwOC1HWHE0VnZrOT90eXBlPXN0aWNrZXImc3RrPWFuaW0mc2lkPTMyMTI4MjMxJnBrZz0zMDk5MzEyCgpObyBBbmltYXRpb246CmxpbmU6Ly9hcHAvMTYwMjY4NzMwOC1HWHE0VnZrOT90eXBlPXN0aWNrZXImc3RrPW5vYW5pbSZzaWQ9MzIxMjgyMzEmcGtnPTMwOTkzMTIKCnR5cGUgPT4gc3RpY2tlcgpzdGsgPT4gYW5pbSAvIG5vYW5pbQpzaWQgPT4gc3RpY2tlciBpZApwa2cgPT4gcGFja2FnZXMgaWQ=')
-            }]).then(function () {
-                liff.closeWindow();
-            });
-        });
-    } else {
-        makeText();
-        makeImage();
-        makeVideo();
-        makeAudio();
-        makeSticker();
-        meProfile();
-    }
-    }
+function initializeApp(data) {
+    document.getElementById('languagefield').textContent = data.language;
+    document.getElementById('viewtypefield').textContent = data.context.viewType;
+    document.getElementById('useridfield').textContent = data.context.userId;
+    document.getElementById('utouidfield').textContent = data.context.utouId;
+    document.getElementById('roomidfield').textContent = data.context.roomId;
+    document.getElementById('groupidfield').textContent = data.context.groupId;
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
-function getProfile(){
-    liff.getProfile().then(function (profile) {
-        document.getElementById('userid').textContent = 'Hai  ' + profile.displayName;
-        document.getElementById('main').src = profile.pictureUrl;        
-        document.getElementById('close').addEventListener('click', function () {
-            liff.closeWindow();
+    // openWindow call
+    document.getElementById('openwindowbutton').addEventListener('click', function () {
+        liff.openWindow({
+            url: 'https://line.me'
         });
     });
-}
 
-function makeText(){
-    var tipe = getParameterByName('type');
-    if (tipe === 'text') {
+    // closeWindow call
+    document.getElementById('closewindowbutton').addEventListener('click', function () {
+        liff.closeWindow();
+    });
+
+    // sendMessages call
+    document.getElementById('sendmessagebutton').addEventListener('click', function () {
         liff.sendMessages([{
             type: 'text',
-            text: getParameterByName('text')
+            text: "You've successfully sent a message! Hooray!"
+        }, {
+            type: 'sticker',
+            packageId: '2',
+            stickerId: '144'
         }]).then(function () {
-            liff.closeWindow();
+            window.alert("Message sent");
+        }).catch(function (error) {
+            window.alert("Error sending message: " + error);
         });
-    }
-}
-
-function makeImage(){
-    var tipe = getParameterByName('type');
-    if (tipe === 'image') {
-        liff.sendMessages([{
-            type: 'image',
-            originalContentUrl: getParameterByName('img'),
-            previewImageUrl: getParameterByName('img')
-        }]).then(function () {
-            liff.closeWindow();
-        });
-    }
-}
-
-function makeVideo(){
-    var tipe = getParameterByName('type');
-    if (tipe === 'video') {
-        liff.sendMessages([{
-            type: 'video',
-            originalContentUrl: getParameterByName('ocu'),
-            previewImageUrl: getParameterByName('piu')
-        }]).then(function () {
-            liff.closeWindow();
-        });
-    }
-}
-
-function makeAudio(){
-    var tipe = getParameterByName('type');
-    if (tipe === 'audio') {
-        liff.sendMessages([{
-            type: 'audio',
-            originalContentUrl: getParameterByName('link'),
-            duration: 60000
-        }]).then(function () {
-            liff.closeWindow();
-        });
-    }
-}
-
-function makeSticker(){
-    var tipe = getParameterByName('type');
-    if (tipe === 'sticker') {
-        var stk = getParameterByName('stk');
-        var sid = getParameterByName('sid');
-        var pkg = getParameterByName('pkg');
-        var ep = '';
-        if (stk === 'anim') {
-            ep = "/IOS/sticker_animation@2x.png";
-        } else {
-            ep = "/IOS/sticker@2x.png";
-        }
-        liff.sendMessages([{
-          type: "template",
-          altText: "Sticker",
-          template: {
-             type: "image_carousel",
-             columns: [{
-                 imageUrl: "https://stickershop.line-scdn.net/stickershop/v1/sticker/"+sid+ep,
-                 action: {
-                     type: "uri",
-                     uri: "line://shop/sticker/detail/"+pkg}}
-                          ]
-                        }
-        }]).then(function () {
-            liff.closeWindow();
-        });
-    }
-}
-
-function meProfile(){
-    var tipe = getParameterByName('type');
-    liff.getProfile().then(function (prof) {
-        var stat = prof.statusMessage;
-        if (stat == null) {
-            var stat = " - ";
-        }
-        if (stat.length > 60) {
-            var stat = "Status Message is to long! Max 60 words";
-        }
-        if (tipe === 'profile') {
-            liff.sendMessages([{
-                type: "template",
-                altText: "Profile "+prof.displayName,
-                template: {
-                    type: "buttons",
-                    thumbnailImageUrl: prof.pictureUrl,
-                    imageAspectRatio: "square",
-                    imageSize: "cover",
-                    title: prof.displayName,
-                    text: stat,
-                    actions: [
-                        {
-                            type:"uri",
-                            label:"Me",
-                            uri:"line://app/1653536815-WyNbrkGr?type=profile"
-                        }
-                    ]
-                }
-            }]).then(function () {
-                liff.closeWindow();
-            });
-        }
     });
+
+    // get access token
+    document.getElementById('getaccesstoken').addEventListener('click', function () {
+        const accessToken = liff.getAccessToken();
+        document.getElementById('accesstokenfield').textContent = accessToken;
+        toggleAccessToken();
+    });
+
+    // get profile call
+    document.getElementById('getprofilebutton').addEventListener('click', function () {
+        liff.getProfile().then(function (profile) {
+            document.getElementById('useridprofilefield').textContent = profile.userId;
+            document.getElementById('displaynamefield').textContent = profile.displayName;
+
+            const profilePictureDiv = document.getElementById('profilepicturediv');
+            if (profilePictureDiv.firstElementChild) {
+                profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
+            }
+            const img = document.createElement('img');
+            img.src = profile.pictureUrl;
+            img.alt = "Profile Picture";
+            profilePictureDiv.appendChild(img);
+
+            document.getElementById('statusmessagefield').textContent = profile.statusMessage;
+            toggleProfileData();
+        }).catch(function (error) {
+            window.alert("Error getting profile: " + error);
+        });
+    });
+}
+
+function toggleAccessToken() {
+    toggleElement('accesstokendata');
+}
+
+function toggleProfileData() {
+    toggleElement('profileinfo');
+}
+
+function toggleElement(elementId) {
+    const elem = document.getElementById(elementId);
+    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
+        elem.style.display = "none";
+    } else {
+        elem.style.display = "block";
+    }
 }
