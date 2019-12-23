fc1bed297968cdeb786a71c76be4e52e87258756
Pratan naimjoi <52064201+tanpattaya@users.noreply.github.com>
2019-12-23 21:37:36 +0700

    Delete index.js

--- แอป liff/index.js
+++
@@ -1,8 +0,0 @@
-function helloWorld() {
-  console.log('Hello World!');
-}
-module.exports = {
-  helloWorld: helloWorld
-}
-const myPackage = require('@codertocat/hello-world-npm');
-myPackage.helloWorld();
